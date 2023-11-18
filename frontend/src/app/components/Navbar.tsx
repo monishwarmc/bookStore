fab5c07bce31ddacd6d030ccd397050285a38c44
Monishwar M C <113018871+monishwarmc@users.noreply.github.com>
2023-11-18 06:17:26 +0530

    Update github

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -1,10 +1,16 @@
-
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div className="w-full bg-sky-500 flex items-center justify-center"
-    ><h1 className="text-6xl font-bold p-3"
-    >Book Store</h1>
+    >
+    <Link href="https://example.com">
+      <a target="_blank" rel="noopener noreferrer"
+        className="hover:bg-blue-500">
+        <h1 className="text-6xl hover:text-5xl font-bold p-3"
+        >Book Store</h1>
+      </a>
+    </Link>
     </div>
   )
 }
