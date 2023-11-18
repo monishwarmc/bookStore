ad678ddad2002074f7684e531f1ac45627c6a19f
Monishwar M C <113018871+monishwarmc@users.noreply.github.com>
2023-11-18 06:34:14 +0530

    Update Navbar.tsx mm

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="w-full bg-sky-500 flex items-center justify-center"
     >
     <Link href="https://github.com/monishwarmc/bookStore" passHref>
-      <a target="_blank" rel="noopener noreferrer"
+      <a target="_blank"
         className="hover:text-blue-900">
         <h1 className="text-6xl hover:underline font-bold p-3"
         >Book Store</h1>
